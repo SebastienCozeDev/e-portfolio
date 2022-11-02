79bff64a0fddaa0223d9d41f07c60bb039aa1232
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-11-02 16:21:12 +0100

    Mise en production.

---
+++ js/removeAuthorImage.js
@@ -0,0 +1,3 @@
+const element = document.getElementById('author-image');
+if (window.innerWidth <= 1024)
+    element.remove();
---
+++ js/removeElementForDesktop.js
@@ -0,0 +1,3 @@
+const element = document.getElementById('only-for-phone-hr');
+if (window.innerWidth > 1024)
+    element.remove();
